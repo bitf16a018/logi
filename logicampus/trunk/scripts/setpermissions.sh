f022aa435f06a281ab73297fc1393e41fa5fee8a
zorka <zorka@6eb2142d-0950-0410-ae72-a70787517d56>
2003-11-23 13:05:36 +0000

    Added a few more options. It now works like this:
    sh setpermissions.sh [path] [web-server-username]
    
    The web-server-username is used to chown the directories to the web server.  The
    permissions are set to 775.  So using it like this would result in:
    
    sh setpermissions.sh /var/www www-data
    
    drwxrwxr-x   3 www-data	staff   102 22 Nov 22:26 cache
    drwxrwxr-x   3 www-data	staff   102 22 Nov 22:26 classLibrary
    drwxrwxr-x  11 www-data	staff   374 22 Nov 22:26 content
    
    I also put in a disclaimer that the person has to agree to before it gets run.

--- logicampus/trunk/scripts/setpermissions.sh
+++ logicampus/trunk/scripts/setpermissions.sh
@@ -1,18 +1,57 @@
 #!/bin/sh
 
-if [ ! $1 ]
+clear
+echo
+echo
+echo
+echo "THIS IS AN EXPERIMENTAL SCRIPT USE AT YOUR OWN RISK"
+echo "RUNNING THIS SCRIPT MAY LEAVE YOUR SERVER IN AN INSECURE"
+echo "STATE."
+echo
+echo "Do you wish to continue? "
+echo -n "[yes/no]:$c "
+read cc
+
+if [ $cc == "no" ] 
 then
-  echo "Please supply the path to your LogiCampus installtion"
+	echo "STOPPING"
+	exit 1
 fi
 
+if [ ! $1 ]
+then
+  echo 
+  echo "Please supply the path to your LogiCampus installtion"
+  echo "Example: /var/www  (no trailing slash)"
+  echo "Example Usaage:  sh setpermissions.sh [path] [www-server-username]"
+  echo
+  exit 1
+fi
+
+if [ ! $2 ]
+then
+	echo
+	echo "Please supply the username your web server is running as"
+    echo "Example Usaage:  sh setpermissions.sh [path] [www-server-username]"
+	echo
+	exit 1
+fi
+
+echo
+echo
 echo "!!!!!!!!!!!!!!!!!!!!!! NOTICE !!!!!!!!!!!!!!!!!!!!!!"
 echo "This script makes the directories writable for your "
-echo "webserver, but does it in a somewhat insecure manner"
-echo "by using chmod 777. You may want to fix this.       "
+echo "webserver."
 echo "!!!!!!!!!!!!!!!!!!!!!! NOTICE !!!!!!!!!!!!!!!!!!!!!!"
+echo 
+echo
 
-chmod -R 777 $1/logicreate/content
-chmod -R 777 $1/logicreate/cache
-chmod -R 777 $1/logicreate/classLibrary
-chmod -R 777 $1/public_html/images
+chmod -R 775 $1/logicreate/content
+chmod -R 775 $1/logicreate/cache
+chmod -R 775 $1/logicreate/classLibrary
+chmod -R 775 $1/public_html/images
 
+chown -R $2  $1/logicreate/content
+chown -R $2  $1/logicreate/cache
+chown -R $2  $1/logicreate/classLibrary
+chown -R $2  $1/public_html/images
