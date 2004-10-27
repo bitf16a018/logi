5e5d7c04b33f9f1961d2d14c80208e7ec8f43c68
Arslan Ahmad <47880584+bitf16a018@users.noreply.github.com>
2004-10-27 04:29:26 +0000

    Removing alter table statement from a create table script

--- logicampus/trunk/src/logicreate/services/pm/META-INFO/setup.sql
+++ logicampus/trunk/src/logicreate/services/pm/META-INFO/setup.sql
@@ -9,6 +9,7 @@ CREATE TABLE privateMessages (
   subject varchar(75) NOT NULL default '',
   repliedTime int(11) NOT NULL default '0',
   sentReceived int(11) NOT NULL default '0',
+  emailNotify VARCHAR(5) NOT NULL,
   PRIMARY KEY  (pkey)
 ) TYPE=MyISAM;
 
@@ -16,5 +17,4 @@ INSERT INTO lcConfig VALUES ('pm', '_displayPerPage', '10', 'text','');
 
 INSERT INTO lcRegistry VALUES ('pm', 'pm', 'Private Messaging', 'MGK', '2002', '||', NOW() , 1);
 INSERT INTO lcConfig VALUES ('pm', '_SystemAdmin', 'zorka', 'text', '');
-ALTER TABLE profile ADD emailNotify VARCHAR(5) NOT NULL;
 INSERT INTO lcPerms VALUES ('reg', 'pm', 'access');
