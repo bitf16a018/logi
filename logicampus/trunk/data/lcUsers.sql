2dbcb46bef15381348bd5b8678fca6946d18b214
mgkimsal <mgkimsal@6eb2142d-0950-0410-ae72-a70787517d56>
2004-11-27 22:30:48 +0000

    Pared down initial users to be more manageable.

--- logicampus/trunk/data/lcUsers.sql
+++ logicampus/trunk/data/lcUsers.sql
@@ -1,25 +1,9 @@
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (1, 'admin', md5('admin'), 'admin@admin.com', '|admin|public|reg|', '2003-11-06 14:55:54', 1);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (3, 'teacher', md5('teacher'), 'teacher@email.com', '|public|reg|faculty|', '2003-11-06 15:12:19', 3);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (99, 'student', md5('student'), 'student@email.com', '|public|reg|student|', '2003-11-06 15:13:40', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (4, 'student1', md5('student1'), 'student1@email.com', '|public|reg|student|', '2003-11-06 15:13:40', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (5, 'staff', md5('staff'), 'staff@email.com', '|hdadmin|hdstaff|public|reg|', '2003-11-06 15:14:22', 1);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (6, 'student2', md5('student2'), 'stu2@email.com', '|public|reg|student|', '2003-11-06 16:20:26', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (7, 'student3', md5('student3'), 'stu3@email.com', '|public|reg|student|', '2003-11-06 16:21:55', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (8, 'student4', md5('student4'), 'stu4@email.com', '|public|reg|student|', '2003-11-06 16:22:30', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (9, 'student5', md5('student5'), 'stu5@email.com', '|public|reg|student|', '2003-11-06 16:23:20', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (10, 'student6', md5('student6'), 'st6@email.com', '|public|reg|student|', '2003-11-06 16:24:03', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (11, 'student7', md5('student7'), 'stu7@email.com', '|public|reg|student|', '2003-11-06 16:24:42', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (12, 'student8', md5('student8'), 'stu8@email.com', '|public|reg|student|', '2003-11-06 16:25:14', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (21, 'admin1', md5('admin1'), 'admin1@email.com', '|admin|faculty|public|reg|', '2003-11-11 11:56:47', 3);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (22, 'admin2', md5('admin2'), 'admin2@email.com', '|public|reg|admin|', '2003-11-11 11:58:20', 1);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (23, 'admin3', md5('admin3'), 'admin3@email.com', '|public|reg|admin|', '2003-11-11 11:59:56', 1);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (24, 'admin4', md5('admin4'), 'admin4@email.com', '|public|reg|admin|', '2003-11-11 12:00:26', 1);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (25, 'admin5', md5('admin5'), 'admin5@email.com', '|public|reg|admin|', '2003-11-11 12:00:52', 1);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (26, 'admin6', md5('admin6'), 'admin6@email.com', '|public|reg|admin|', '2003-11-11 12:01:22', 1);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (27, 'admin7', md5('admin7'), 'admin7@email.com', '|admin|public|reg|', '2003-11-11 12:58:15', 1);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (28, 'admin8', md5('admin8'), 'admin8@email.com', '|public|reg|admin|', '2003-11-11 13:00:11', 1);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (29, 'student9', md5('student9'), 'student9@email.com', '|reg|student|public|', '2003-11-12 09:11:20', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (30, 'student10', md5('student10'), 'student10@email.com', '|reg|student|public|', '2003-11-12 09:11:20', 2);
-INSERT INTO lcUsers (pkey, username, password, email, groups, createdOn, userType) VALUES (31, 'student11', md5('student11'), 'student11@email.com', '|reg|student|public|', '2003-11-12 09:11:20', 2);
-
+INSERT INTO lcUsers VALUES (1,'admin','21232f297a57a5a743894a0e4a801fc3','admin@admin.com','|admin|public|reg|','2004-11-27 17:26:49',1);
+INSERT INTO lcUsers VALUES (2,'teacher1','41c8949aa55b8cb5dbec662f34b62df3','teacher1@email.com','|public|reg|faculty|','2004-11-27 17:26:49',3);
+INSERT INTO lcUsers VALUES (3,'teacher2','ccffb0bb993eeb79059b31e1611ec353','teacher2@email.com','|public|reg|faculty|','2004-11-27 17:26:49',3);
+INSERT INTO lcUsers VALUES (4,'staff','1253208465b1efa876f982d8a9e73eef','staff@email.com','|hdadmin|hdstaff|public|reg|','2004-11-27 17:26:49',1);
+INSERT INTO lcUsers VALUES (6,'ralph.wiggum','336137dff3be08a4b71f77abef46fe73','ralph.wiggum@email.com','|public|reg|student|','2004-11-27 17:26:49',2);
+INSERT INTO lcUsers VALUES (7,'lisa.simpson','6cebe4044588b8c9cbe1de92da02dea2','lisa.simpson@email.com','|public|reg|student|','2004-11-27 17:26:49',2);
+INSERT INTO lcUsers VALUES (8,'bart.simpson','c1c41bed296920eb65a3a3a5d45fa1ff','bart.simpson@email.com','|public|reg|student|','2004-11-27 17:26:49',2);
+INSERT INTO lcUsers VALUES (9,'nelson.muntz','84b37601db41fa18779f4de4fdeb4d14','nelson.muntz@email.com','|public|reg|student|','2004-11-27 17:26:49',2);
 
