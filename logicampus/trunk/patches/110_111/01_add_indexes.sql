d15ad1f714a771abac549daad44c0e43206cc893
Arslan Ahmad <47880584+bitf16a018@users.noreply.github.com>
2005-07-19 19:22:21 +0000

    Script to add new indexes

---
+++ logicampus/trunk/patches/110_111/01_add_indexes.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `class_links` ADD INDEX `id_class_links_categories` ( `id_class_links_categories` );
+ALTER TABLE `class_links` ADD INDEX `id_classes` ( `id_classes` );
+
+ALTER TABLE `class_group` ADD INDEX `class_id` ( `class_id` );
+
+ALTER TABLE `class_presentations` ADD INDEX `id_classes` ( `id_classes` );
+
+ALTER TABLE `class_assignments_grades` ADD INDEX `id_class_assignments` ( `id_class_assignments` );
+ALTER TABLE `class_assignments_link` ADD INDEX `id_class_lessons` ( `id_class_lessons` );
+ALTER TABLE `class_assignments_link` ADD INDEX `id_class_assignments` ( `id_class_assignments` );
