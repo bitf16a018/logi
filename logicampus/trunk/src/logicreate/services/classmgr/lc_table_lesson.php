48013b3814404b2e11125ed6a3a1a3d0376f8c96
Arslan Ahmad <47880584+bitf16a018@users.noreply.github.com>
2005-07-19 15:48:40 +0000

    Saves Objectives.

---
+++ logicampus/trunk/src/logicreate/services/classmgr/lc_table_lesson.php
@@ -0,0 +1,78 @@
+<?php
+
+
+class LinkedItemsModel extends LC_TableModel {
+
+	var $sql = '';
+	var $results = array();
+	var $page = 1;
+	var $resultsPerPage = 100;
+	var $maxResults;
+	var $columnNames = array();
+
+
+	/**
+	 * Get sql results 
+	 */
+	function LinkedItemsModel($sql,$db) {
+		$db->query($sql);
+		while ($db->next_record()) {
+			$this->results[] = $db->Record;
+		}
+	}
+
+	function setColumnNames($array) {
+		$this->columnNames = $array;
+	}
+
+
+	//sub-class
+	/**
+	 * Returns the number of rows in the model.
+	 */
+	function getRowCount() {
+		return (count($this->results));
+	}
+
+
+	/**
+	 * Returns the number of cols in the model.
+	 */
+	function getColumnCount() {
+		return 2;
+	}
+
+
+	/**
+	 * Returns the name of a column.
+	 */
+	function getColumnName($columnIndex) {
+
+		return $this->columnNames[$columnIndex];
+	}
+
+
+	/**
+	 * return the value at an x,y coord
+	 */
+	function getValueAt($x,$y) {
+		$record = $this->results[$x];
+		if ($y == 0 ) { return $record; }
+		return $record[$y];
+	}
+}
+
+
+
+class LC_TableCheckboxRenderer extends LC_TableCellRenderer {
+
+	var $selectedVal;
+	var $selectedKey;
+
+	function getRenderedValue() {
+		$selected = ($this->selectedVal == $this->value[$this->selectedKey]) ? ' CHECKED ':'';
+		return '<input name="item['.$this->row.']" value="'.$this->value['id_class_objectives'].'" '.$selected.' type="checkbox">';
+	}
+}
+
+?>
