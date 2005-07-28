b1449f9ca0aa95059b9263983309287e286f9faf
Arslan Ahmad <47880584+bitf16a018@users.noreply.github.com>
2005-07-28 15:39:35 +0000

    small updates.

--- logicampus/trunk/unittest/testCase/unitTest.php
+++ logicampus/trunk/unittest/testCase/unitTest.php
@@ -1,17 +1,19 @@
 <?
 
-class unitTest {
+class unitTest extends NoAuth {
 
 	var $test = array();
 	var $results = array();
+	var $presentor = 'EmptyPresentation';
+	var $authorizer = 'noAuth';
 
 	function run(&$db, &$u, &$lc, &$t) {
 
 		$lc->templateName = 'unitTest';
 		while ( list($k,$v ) = @each($this->tests) ) {
 			$test = '$this->results[\''.$k.'\'] = $this->'.$v['name'].'Test();';
+			eval($test);
 			//print "testing $k $v <br>";
-			//eval($test);
 		}
 
 
@@ -23,5 +25,28 @@ class unitTest {
 		$t['m'] = 'testCase';
 	}
 
+	/**
+	 * show details about one particular test case
+	 */
+	/*
+	function inspectRun(&$db, &$u, &$lc, &$t) {
+
+		$testName = $lc->getvars['t'];
+		$moduleName = $lc->getvars['m'];
+		$serviceName = $lc->getvars['s'];
+		$file = fopen(SERVICES_PATH.$moduleName.'/'.$serviceNames,'r+');
+		print "<pre>\n";
+		while (!feof($file) ) {
+			$line = fgets($file);
+			while(list($k,$v) = each($this->tests) ) {
+				if ( strpos($line,$v['name']) ) {
+					echo '<a name="'.$k.'"/>';
+				}
+			}
+			echo $line . " == ";
+		}
+		print "</pre>\n";
+	}
+//	*/
 }
 ?>
