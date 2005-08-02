baa537b8ca34dbc75e024f7ed396ed3d46fda72a
hardcorelamer <hardcorelamer@6eb2142d-0950-0410-ae72-a70787517d56>
2005-08-02 13:10:45 +0000

    Adding assessment lookup indexes

--- logicampus/trunk/patches/110_111/01_add_indexes.sql
+++ logicampus/trunk/patches/110_111/01_add_indexes.sql
@@ -8,3 +8,7 @@ ALTER TABLE `class_presentations` ADD INDEX `id_classes` ( `id_classes` );
 ALTER TABLE `class_assignments_grades` ADD INDEX `id_class_assignments` ( `id_class_assignments` );
 ALTER TABLE `class_assignments_link` ADD INDEX `id_class_lessons` ( `id_class_lessons` );
 ALTER TABLE `class_assignments_link` ADD INDEX `id_class_assignments` ( `id_class_assignments` );
+
+ALTER TABLE `assessment` ADD INDEX ( `class_id` );
+ALTER TABLE `assessment` ADD INDEX ( `date_available` );
+ALTER TABLE `assessment` ADD INDEX ( `date_unavailable` );
