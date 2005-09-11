93f1669160381354da1a871d25ccaf2d22920da6
hardcorelamer <hardcorelamer@6eb2142d-0950-0410-ae72-a70787517d56>
2005-09-11 18:49:34 +0000

    Adding web stats feedback function to installer.

--- logicampus/trunk/src/public_html/install.php
+++ logicampus/trunk/src/public_html/install.php
@@ -11,6 +11,7 @@
 
 <br/>
 
+<form method="GET" style="padding:0px;margin:0px;" action="<?php echo $PHP_SELF;?>">
 <fieldset style="background-color:#DDE;font-family:Helvetica,Arial;">
 <legend style="font-size:120%;">Instructions</legend>
 	<p>
@@ -41,13 +42,117 @@
 </fieldset>
 
 <br/>
+<a href="<?php echo $PHP_SELF;?>">Click here</a> to reload the page when you're done with the installation.
+<br/>
+<br/>
+<?php
+if ($_GET['send'] == 'go') {
+	$stats = true;
+	if ( !sendStats('logicampus.com') ) {
+		$stats = sendStats('216.40.247.38');
+	}
 
-<a href="<?= $PHP_SELF;?>">Click here</a> to reload the page when you're done with the installation.
+	if ($stats) {
+		echo '<h3>Thanks for sending your data.</h3>';
+	} else {
+		echo '<h3>There was a problem sending your data.</h3>';
+	}
+}
+
+?>
+
+<fieldset style="background-color:#DDE;font-family:Helvetica,Arial;">
+<legend style="font-size:120%;">Statistics</legend>
+	<p>
+	You may report this installation by clicking on the submit button below.
+	No personally identifiable information is required.  The information that will 
+	be submitted automatically is:
+
+		<ol>
+			<li>Web Server Name and/or IP Address: <?php echo $_SERVER['HTTP_HOST'].'/'.$_SERVER['SERVER_ADDR'];?></li>
+			<li>Browser Type: <?php echo $_SERVER['HTTP_USER_AGENT'];?></li>
+			<li>Campus Version: @version.number@@version.extra@</li>
+			<li>PHP Version: <?php echo PHP_VERSION;?></li>
+			<li>Today's Date: <?php echo  date('m.d.Y');?></li>
+		</ol>
+	</p>
+
+	<input type="submit" id="submit_button" value="Don't Send Personal Info."/>
+	<p>
+	In addition, you may also add personal information if you wish:
+	<div style="width:300px;text-align:right">
+		<ol>
+			<li style="float:left;"><label for="org_name">
+				Organization Name:</label></li>
+			<input id="org_name" type="text" size="35" value=""/>
+			<li style="float:left;"><label for="contact_name">
+				Contact Name:</label></li>
+		       	<input id="contact_name" type="text" size="35" value=""/>
+			<li style="float:left;"><label for="contact_email">
+				Contact Email:</label></li> 
+			<input id="contact_name" type="text" size="35" value=""/>
+			<li style="float:left;"><label for="contact_phone">
+				Contact Phone Number:</label></li>
+			<input type="text" size="35" value=""/>
+		</ol>
+	</div>
+	<input type="hidden" name="send" value="go"/>
+	<input type="submit"  id="submit_button" value="Send Statistics"/>
+	</p>
+
+</fieldset>
+</form>
+
+<br/>
 
 <div style="margin-top:2em;font-size:75%;">
-LogiCampus <?= LOGICAMPUS_VERSION.LOGICAMPUS_VERSION_STATUS;?>
+LogiCampus <?php echo LOGICAMPUS_VERSION.LOGICAMPUS_VERSION_STATUS;?>
 <br/>
-Build Date: <?= LOGICAMPUS_BUILD_DATE;?>
+Build Date: <?php echo LOGICAMPUS_BUILD_DATE;?>
 </div>
+<pre>
+<?php //print_r($_SERVER);?>
+</pre>
 </body>
 </html>
+<?php
+	//define function to send stat info
+	function sendStats($host) {
+		$post = 'http_host='.$_SERVER['HTTP_HOST'];
+		$post .="\n";
+		$post .= '&server_addr='.$_SERVER['SERVER_ADDR'];
+		$post .="\n";
+		$post .= '&user_agent='.$_SERVER['HTTP_USER_AGENT'];
+		$post .="\n";
+		$post .= '&campus_version=@version.number@@version.extra@';
+		$post .="\n";
+		$post .= '&php_version='.$PHP_VERSION;
+		$post .="\n";
+		$post .= '&install_date='.time();
+
+		//echo $post;
+
+
+		$fp = fsockopen ($host, 80);
+		if ($fp) {
+
+			fputs ($fp, "
+POST /web_reg.php HTTP/1.0
+Host: logicampus.com
+Content-Length: ".strlen($post)."
+Content-Type: application/x-www-form-urlencoded
+Connection: Close
+
+".$post."
+
+");
+
+
+			while (!feof($fp)) {fgets ($fp,4096); }
+			 fclose ($fp);
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+?>
