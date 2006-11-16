798ae0b759c32804bb1c4a7553906a51c3df4cfb
hardcorelamer <hardcorelamer@6eb2142d-0950-0410-ae72-a70787517d56>
2006-11-16 20:36:50 +0000

    Adding support for multiple template styles.

---
+++ logicampus/trunk/src/public_html/templates/sinorca/header.html.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en-AU">
+  <head>
+    <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+    <meta name="author" content="haran" />
+    <meta name="generator" content="author" />
+
+    <!-- Navigational metadata for large websites (an accessibility feature): -->
+    <link rel="top"      href="./index.html" title="Homepage" />
+    <link rel="up"       href="./index.html" title="Up" />
+    <link rel="first"    href="./index.html" title="First page" />
+    <link rel="previous" href="./index.html" title="Previous page" />
+    <link rel="next"     href="./index.html" title="Next page" />
+    <link rel="last"     href="./index.html" title="Last page" />
+    <link rel="toc"      href="./index.html" title="Table of contents" />
+    <link rel="index"    href="./index.html" title="Site map" />
+
+    <link rel="stylesheet" type="text/css" href="<?=TEMPLATE_URL;?>sinorca-screen.css" media="screen" title="Sinorca (screen)" />
+    <link rel="stylesheet alternative" type="text/css" href="sinorca-screen-alt.css" media="screen" title="Sinorca (alternative)" />
+    <link rel="stylesheet" type="text/css" href="sinorca-print.css" media="print" />
+
+    <title>Sinorca 2.0</title>
+  </head>
+
+  <body>
+    <!-- For non-visual user agents: -->
+      <div id="top"><a href="#main-copy" class="doNotDisplay doNotPrint">Skip to main content.</a></div>
+
+    <!-- ##### Header ##### -->
+
+    <div id="header">
+      <div class="superHeader">
+      <!--
+        <div class="left">
+          <span class="doNotDisplay">Related sites:</span>
+          <a href="./index.html">Link 1</a> |
+          <a href="./index.html">Link 2</a>
+        </div>
+        <div class="right">
+          <span class="doNotDisplay">More related sites:</span>
+          <a href="./index.html">Link 3</a> |
+          <a href="./index.html">Link 4</a> |
+          <a href="./index.html">Link 5</a> |
+          <a href="./index.html">Link 6</a> |
+          <a href="./index.html">Link 7</a>
+        </div>
+	  -->
+      </div>
+
+      <div class="midHeader">
+        <h1 class="headerTitle">LogiCampus</h1>
+      </div>
+
+      <div class="subHeader">
+        <span class="doNotDisplay">Navigation:</span>
+        <a href="./index.html">Home</a> |
+        <a href="./index.html" class="highlight">Sinorca 2.0</a> |
+        <a href="./index.html">Other</a> |
+        <a href="./index.html">Header</a> |
+        <a href="./index.html">Links</a>
+      </div>
+    </div>
+
+    <!-- ##### Side Bar ##### -->
+
+    <div id="side-bar">
+      <div>
+	<?php
+		if ($t['_classesTaught'] != '') { 
+	?>
+        	<p class="sideBarTitle"><?=lct('classroom manager')?></p>
+		<a class="menuitem" href="<?=APP_URL?>'classmgr"><?=lct('Faculty Overview')?></a>
+		<ul>
+	<?php
+		foreach($obj->user->classesTaught as $k => $v )
+		{
+			echo '<li><a class="menuitem" href="'.APP_URL.'classmgr/display/id_classes='.$v->id_classes.'">&rsaquo;&nbsp;'.$v->courseFamily.$v->courseNumber.' ('.$v->semesterID.')</a>';
+			echo '<a class="menuitem" href="'.APP_URL.'gradebook/main/id_classes='.$v->id_classes.'">&nbsp;&nbsp;&rsaquo;&nbsp;'.lct('Gradebook').'</a></li>';
+		}
+	?>
+		</ul>
+	<?php
+
+		}
+	?>
+      </div>
+
+
+      <div>
+	<?php
+		if ($t['_classesTaken'] != '') { 
+	?>
+        	<p class="sideBarTitle"><?=lct('my classes')?></p>
+		<a class="menuitem" href="'.appurl('classroom/').'"><?=lct('Classroom Portal');?></a>
+		<ul>
+	<?php
+
+		foreach($obj->user->classesTaken as $k => $v ) {
+			if ($v->semesterID) { 
+			echo '<li><a class="menuitem" href="'.APP_URL.'classroom/details/id_classes='.$v->id_classes.'">&raquo;&nbsp;'.$v->courseFamily.$v->courseNumber.' ('.$v->semesterID.')</a>';
+			} else { 
+			echo '<li><a class="menuitem" href="'.APP_URL.'classroom/details/id_classes='.$v->id_classes.'">&raquo;&nbsp;'.$v->courseFamily.$v->courseNumber.'</a>';
+			}
+			echo '<a class="menuitem" href="'.APP_URL.'classroom/gradebook/id_classes='.$v->id_classes.'">&nbsp;&nbsp;&raquo;&nbsp;'.lct('Gradebook').'</a></li>';
+		}
+	?>
+		</ul>
+	<?php
+
+		}
+	?>
+      </div>
+
+
+      <div class="lighterBackground">
+        <p class="sideBarTitle">Alternative stylesheet</p>
+        <span class="sideBarText">
+          Sinorca contains an alternative stylesheet that renders this side bar differently.
+        </span>
+        <span class="sideBarText">
+          To view the alternative stylesheet in (certain)
+          <span class="titleTip" title="eg: Mozilla, Camino, Netscape 6+">Gecko-based browsers</span>,
+          select <em>Sinorca (alternative)</em> from the <em>View</em> &rsaquo; <em>Use Style</em>
+          submenu.
+        </span>
+      </div>
+    
+      <div>
+        <p class="sideBarTitle">Validation</p>
+        <span class="sideBarText">
+          Validate the <a href="http://validator.w3.org/check/referer">XHTML</a> and
+          <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> of this page.
+        </span>
+      </div>
+    </div>
+
+    <!-- ##### Main Copy ##### -->
+
+    <div id="main-copy">
+
