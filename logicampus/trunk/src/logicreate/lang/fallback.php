09b0d2baab8bef5225116108a60de9cc044cd23e
Arslan Ahmad <47880584+bitf16a018@users.noreply.github.com>
2006-12-16 00:32:28 +0000

    fixing fallback language to properly handle arguments.

--- logicampus/trunk/src/logicreate/lang/fallback.php
+++ logicampus/trunk/src/logicreate/lang/fallback.php
@@ -1,8 +1,21 @@
 <?
 
 function lct($key,$args="") {
-	//extract($args);
+	extract($args);
 
-	return "$key";
+	$newArgs = array();
+	$newArgs[] = $key;
+	$format = "%s";
+	//only support 3 args by default.
+	for ($x=0; $x<4;$x++) {
+		$qq = current($args);
+		if (! strlen($qq) ) {
+			break;
+		}
+		$newArgs[] = current($args);
+		next($args);
+		$format .= " %s";
+	}
+	return vsprintf($format,$newArgs);
 }
 ?>
