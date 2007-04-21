d6eb94c8e1e5359400c705019024fd9777134b3b
Arslan Ahmad <47880584+bitf16a018@users.noreply.github.com>
2007-04-21 15:16:26 +0000

    getting rid of old class student sections table.

--- logicampus/trunk/src/logicreate/services/administration/META-INFO/setup.sql
+++ logicampus/trunk/src/logicreate/services/administration/META-INFO/setup.sql
@@ -1,15 +1,3 @@
-CREATE TABLE class_student_sections (
-  sectionNumber int(11) NOT NULL default '0',
-  id_student varchar(75) NOT NULL default '',
-  semester_id int(11) unsigned NOT NULL default '0',
-  active tinyint(1) NOT NULL default '0',
-  dateWithdrawn date default NULL,
-  UNIQUE KEY section_student_semester (sectionNumber,id_student,semester_id),
-  KEY id_student (id_student),
-  KEY semester_id (semester_id),
-  KEY active (active)
-) TYPE=MyISAM;
-
 
 CREATE TABLE class_sections (
   sectionNumber int(11) NOT NULL default '0',
