2c28d40fe76ef8ea9d84980739111c12a03c1dd1
Arslan Ahmad <47880584+bitf16a018@users.noreply.github.com>
2007-04-23 00:56:29 +0000

    Changing the way the new private message box works, using the new lc message box.

--- logicampus/trunk/src/public_html/templates/private/footer.html.php
+++ logicampus/trunk/src/public_html/templates/private/footer.html.php
@@ -12,13 +12,5 @@
 	</tr>
 </table>
 
-<? if ($t['_newPrivMsgs']) {  ?>
-<script>
-	if ( confirm('You have new private messages. click "OK" to read them.') ) {
-		document.location.href = '<?=appurl('pm');?>';
-	}
-</script>
-<? } ?>
-
 </BODY>
 </HTML> 
--- logicampus/trunk/src/public_html/templates/private/header.html.php
+++ logicampus/trunk/src/public_html/templates/private/header.html.php
@@ -167,4 +167,9 @@
 	if ( count($sysMessages) ) {
 		echo lcMessageBox($sysMessages);
 	}
+
+	if ($t['_newPrivMsgs']) {  
+		echo lcMessageBox('Someone sent you a new private message.  Do you want to read them?  <a href="'.appurl('pm').'">Yes, take me to my private messages.</a>','q');
+	} 
 ?>
+
