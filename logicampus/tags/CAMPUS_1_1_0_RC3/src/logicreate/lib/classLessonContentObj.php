9213091fd85d810d0263f458d8dfae57870f0667
nobody <nobody@6eb2142d-0950-0410-ae72-a70787517d56>
2007-05-10 18:29:09 +0000

    This commit was manufactured by cvs2svn to create tag 'CAMPUS_1_1_0_RC3'.

---
+++ logicampus/tags/CAMPUS_1_1_0_RC3/src/logicreate/lib/classLessonContentObj.php
@@ -0,0 +1,95 @@
+<?
+// primarily mapped against database table 
+class classLessonContentObj {
+	var $id_class_lesson_content;		// (number) - 
+	var $id_classes;		// (number) - 
+	var $txTitle;		// (number) - 
+	var $txText;		// (number) - 
+	var $dateCreated;		// (number) - 
+	var $_dsn = 'default';
+	var $_pkey = 'id_class_lesson_content';
+function _getFromDB($pkey,$prop='',$where='', $orderBy='', $dsn='default') {
+if ($orderBy) { $orderBy = ' order by $orderBy '; }
+if ($where) { $where = " and $where"; }
+$db = DB::getHandle($dsn);
+ if ($prop=='') { $prop=$this->_pkey; }
+$db->query("select id_class_lesson_content,id_classes,txTitle,txText,dateCreated from class_lesson_content where $prop='$pkey' $where $orderBy");
+while ($db->next_record()) {
+ $temp = new classLessonContentObj();
+$temp->_dsn = $dsn;
+$temp->__loaded = true; 
+$temp->id_class_lesson_content = $db->Record['id_class_lesson_content'];
+$temp->id_classes = $db->Record['id_classes'];
+$temp->txTitle = $db->Record['txTitle'];
+$temp->txText = $db->Record['txText'];
+$temp->dateCreated = $db->Record['dateCreated'];
+$objects[] = $temp;
+}
+if (count($objects)>1) {
+ return $objects;
+ } else {
+ return $objects[0]; 
+}
+}
+function _getAllFromDB($dsn='default', $where='', $orderBy='') {
+$db = DB::getHandle($dsn);
+if ($orderBy) { $orderBy = ' order by $orderBy '; }
+if ($where) { $where = ' and $where'; }
+$db->query("select id_class_lesson_content,id_classes,txTitle,txText,dateCreated from class_lesson_content $where $orderBy");
+while ($db->next_record()) {
+ $temp = new classLessonContentObj();
+$temp->_dsn = $dsn;
+$temp->__loaded = true; 
+$temp->id_class_lesson_content = $db->Record['id_class_lesson_content'];
+$temp->id_classes = $db->Record['id_classes'];
+$temp->txTitle = $db->Record['txTitle'];
+$temp->txText = $db->Record['txText'];
+$temp->dateCreated = $db->Record['dateCreated'];
+$objects[] = $temp;
+}
+ return $objects;
+
+}
+
+function _loadArray($array, $pkeyFlag=false) {
+ if ($pkeyFlag) { 
+	$this->id_class_lesson_content = $array['id_class_lesson_content'];
+ }
+	$this->id_classes = $array['id_classes'];
+	$this->txTitle = $array['txTitle'];
+	$this->txText = $array['txText'];
+	$this->dateCreated = $array['dateCreated'];
+}
+
+function _genPkey($len=50) {
+return str_replace(".","",uniqid(str_replace(" ","",microtime()), $len));
+}
+
+function _saveToDB() {
+$db = DB::GetHandle($this->_dsn);
+if ($this->id_class_lesson_content =='') {$this->id_class_lesson_content=$this->_genPkey(); }
+if ($this->__loaded) { // was the object loaded from DB already?
+$sql = "update class_lesson_content set id_class_lesson_content='{$this->id_class_lesson_content}',id_classes='{$this->id_classes}',txTitle='{$this->txTitle}',txText='{$this->txText}',dateCreated='{$this->dateCreated}' where id_class_lesson_content = '{$this->{$this->_pkey}}'";
+$db->query($sql);
+} else {
+$sql = "insert into class_lesson_content   (id_classes,txTitle,txText,dateCreated) values ('{$this->id_classes}','{$this->txTitle}','{$this->txText}','{$this->dateCreated}')";
+$db->query($sql);
+}
+
+}
+
+function _deleteToDB() { return $this->_deleteFromDB(); } 
+
+function _deleteFromDB() {
+$db = DB::GetHandle($this->_dsn);
+if ($this->id_class_lesson_content =='') {  trigger_error('deleteFromDB call with empty key'); return false; }
+if ($this->__loaded) { // was the object loaded from DB already?
+$sql = "delete from class_lesson_content  where id_class_lesson_content = '{$this->{$this->_pkey}}'";
+$db->query($sql);
+
+}
+
+}
+}
+
+?>
---
+++ logicampus/tags/CAMPUS_1_1_0_RC3/src/public_html/index.php
@@ -0,0 +1,345 @@
+<?
+//
+// +----------------------------------------------------------------------+
+// | LogiCreate Application Server sversion 2.9.1                         |
+// | Copyright (c)  2002 tapinternet.com	                              |
+// +----------------------------------------------------------------------+
+// |	                                                                  |
+// +----------------------------------------------------------------------+
+// | Authors: Mark Kimsal      		                                      |
+// |          Michael Kimsal                                              |
+// |          Keith Elder                                                 |
+// |          Ryan Sexton                                                 |
+// +----------------------------------------------------------------------+
+//
+//xdebug_start_profiling();
+
+ob_start();
+
+		/*
+		 * if you're going to time things,
+		 * start the timer as the first step
+		 */
+
+function get_microtime()
+{	$tmp = split(' ', microtime());
+	$ret = $tmp[0]+$tmp[1];
+return $ret;
+}
+$execution_time=get_microtime();
+
+	$start = microtime();
+	srand ((double) microtime() * 1000000);
+
+	include("defines.php");
+
+
+
+
+		/*
+		 * Start session handling
+		 */
+	//keep the login as feature in a seperate cookie so that people can logout
+	if ( @$_COOKIE['LOGINAS'] != '' ) {
+		$PHPSESSID= $_COOKIE['LOGINAS'];
+	}
+	if ( $PHPSESSID=="" )  {		//if no cookie was passed, set a new one
+		$hadCookie = false;
+		$PHPSESSID=md5 (uniqid (rand()));
+		setcookie("PHPSESSID",$PHPSESSID,0,$tail);
+	} else { 
+		$hadCookie = true;
+	}
+
+
+		//
+		//Include main files
+		// create system object
+		//
+
+include(LIB_PATH."LC_include.php");
+include(LIB_PATH."LC_user.php");
+include(LIB_PATH."pellet.php");
+include(LIB_PATH."LC_db.php");
+include_once(LIB_PATH."LC_registry.php");
+include_once(LIB_PATH."lc_SQL.php");
+include_once(LIB_PATH."LC_html.php");
+
+
+
+// ************* I18N ******************************
+//if get vars, switchlocale, then set a cookie
+if ( strlen(@$_GET['switchlocale']) == 5 || strlen(@$_GET['switchlocale']) == 3) {
+	$_COOKIE['locale'] = $_GET['switchlocale'];
+	setcookie('locale',$_GET['switchlocale'],0);
+}
+//determind language choice from cookie
+if ( strlen(@$_COOKIE['locale']) == 5 || strlen(@$_COOKIE['locale']) == 3) {
+	define('LOCALE', $_COOKIE['locale']);
+} else {
+	define('LOCALE', 'en_US');
+}
+
+if (!@include_once(LANG_PATH."lct.".LOCALE.".php") ) {
+	include_once(LANG_PATH."fallback.php");
+}
+// ************* I18N ******************************
+
+
+
+$lcObj = new lcSystem();
+$lcTemplate = array('cssFile'=>'site.css');
+define('HERC',false);
+
+		//
+		//Authenticate the URL
+		//and make PATH_INFO into variables
+		//for others to use via the lcObj
+		//
+
+if ($QUERY_STRING!="") {
+	$QUERY_STRING = ereg_replace("&","/",$QUERY_STRING);
+	$PATH_INFO .= "/$QUERY_STRING";
+}
+
+
+		/*
+		 * Parse the url to find out module information
+		 * possible URL configurations:	
+		 * 	index.php/foo		=> 	module=foo, service=main
+		 *	index.php/foo/main	=>	module=foo, service=main
+		 *	index.php/foo/bar	=>	module=foo, service=bar
+		 *	index.php/foo/bar/lunix	=>	module=foo, service=bar, getvars[0]=lunix
+		 */
+	$url = $_SERVER['PATH_INFO'];
+	if (!$url) { $url = $HTTP_SERVER_VARS['PATH_INFO']; }
+	$name = explode("/",$url);
+	/*
+	 * move to default service if none is specified
+	 */
+	if (($url=="") || ($name[1]=="") || ($url=="/") ) {
+		//add close page functionality for tracking/logging
+		header("Location: ".DEFAULT_URL); 
+		exit();
+	}
+
+	$registry = lcRegistry::load($name[1]);
+
+	$moduleName = $name[1];
+	$serviceName = $name[2];
+	$className = $serviceName;
+
+	if ($className == "main") { $className = $name[1]; }
+	if ($serviceName == "") { $serviceName = "main"; $className = $moduleName;}
+	$gdb->query("select * from lcRegistry where moduleName ='$moduleName'",false);
+	$gdb->next_record();
+	$module = $gdb->Record;
+
+		/*
+		 * parse the get string
+		 * if we find /foo=bar/ then set
+		 * getvars[foo] = bar
+		 * x=3 for no random cache-buster, 
+		 * x=4 if you want a random string in each URL
+		 */
+
+$lcObj->getvars = $HTTP_GET_VARS;
+$lcObj->moduleName = $moduleName;
+$lcObj->serviceName = $serviceName;
+
+
+$x=3;
+while ($x<=sizeof($name)) {
+	$key="";
+	$tempval = trim($name[$x-1]);
+	if (ereg("=",$tempval)) {
+		list($key,$val) = explode("=",$tempval);
+	} else {
+		$val = $tempval;
+	}
+	$val = urldecode($val);
+	$lcObj->getvars[] = $val;
+	if ($key!="") { $lcObj->getvars[$key] = $val; }
+	if (substr($name[$x-2],0,3)=="rd=") {
+		$lcObj->redir = substr($name[$x-1],3);
+	}
+	$x++;
+}
+
+
+		/*
+		 * Include the actual service
+		 * /main.php/disp
+		 * 'module' is 'disp'
+		 * 'service' is 'main'
+		 * so we pull in disp/main.lcp from services/disp/
+		 */
+if ( !include (SERVICE_PATH.$moduleName."/$serviceName.lcp") ) {
+	if (!file_exists(SERVICE_PATH.$moduleName."/$serviceName.lcp") ) {
+		$lcTemplate[message] = "The service <i>$serviceName</i> does not exist.";
+	} else {
+		$lcTemplate[message] = "The service <i>$serviceName</i> cannot be run.  It might contain errors.";
+	}
+	$service = new Service();
+//	lcSystem::systemPreTemplate($lcObj,$lcTemplate);
+	$service->errorMessage($lcObj,$lcTemplate);
+	closepage();
+}
+
+		/*
+		 * Create the user object
+		 */
+
+$lcUser = lcUser::getUserBySesskey($PHPSESSID);
+$lcUser->_sessionKey = $PHPSESSID;
+
+
+# Added for shopping cart to track where orders
+# are coming from.  Also has other uses as well
+# as a standars LC session variable.
+if ($lcUser->sessionvars['_CAMEFROM']=='') { 
+	$lcUser->sessionvars['_CAMEFROM'] = $_SERVER['HTTP_REFERER'];
+}
+
+
+	$lcUser->loadPerms($moduleName);
+	$service = new $className();
+	$authHandle = $service->authorizer;
+	if ($authHandle == '' || $authHandle == 'native') {
+		$authHandle = '$authResponse = $service->authorize($lcObj,$lcUser);';
+	} else {
+		$authHandle = '$authResponse = '.$authHandle.'($lcObj,$lcUser);';
+	}
+
+	eval($authHandle);
+
+	if (! $authResponse ) 
+	{   // default access denied message
+	    $lcTemplate['message'] = 'You do not have permission to view this document.
+	    You may need to <a href="'.APP_URL.'login/">register</a> on this site to
+	    gain access.
+	    If you feel you have gotten this page in error, contact the site
+	    administrator.';
+	        
+	    // checking to see if we should use system default or user editable message
+	    if (file_exists(CONTENT_PATH.'err_access_denied.html'))
+	    {   # I use ob_end and start to maintain backwards compatability of old php v.
+		ob_end_flush();
+		ob_start();
+		@include(CONTENT_PATH.'err_access_denied.html');
+		$lcTemplate['message'] = ob_get_contents();
+		ob_end_clean();
+		ob_start();
+	    }
+
+		lcSystem::systemPreTemplate($lcObj,$lcTemplate);
+		$service->errorMessage($lcObj,$lcTemplate);
+		closepage();
+	}
+
+
+/*
+ * template stuff
+ */
+$lcObj->module_root = SERVICE_PATH."$moduleName/";
+$lcObj->templateName = $serviceName;
+
+$service->serviceName = $serviceName;
+$service->className = $className;
+
+$service->module = $moduleName;
+$service->templateName = $serviceName;
+
+DEFINE(MOD_URL,APP_URL.$moduleName."/");
+DEFINE(SECURE_MOD_URL,SECURE_APP_URL.$moduleName."/");
+DEFINE(MOD_PATH,SERVICE_PATH.$moduleName."/");
+
+
+
+	/*
+	 * get configuration for this module
+	 */
+
+	while ( list($k,$v) = @each($registry->config) ) {
+		$service->{$k} = $v;
+	}
+
+		/*
+		 * If we have an 'event' posted
+		 * assume the event is a function in the 
+		 * service we included
+		 * and execute it (eval)
+		 * event = blah 
+		 * $service->blahRun($params)
+		 */
+
+	$event = $lcObj->postvars["event"];
+	if ($event =="") {
+		$event = $lcObj->getvars["event"];
+	}
+
+$service->event = $event;
+if($event != '' ) {
+	$lcObj->templateName .= '_'.$event;
+}
+
+	if ($event == "") {
+		$_db = DB::getHandle();
+		$service->run($_db,$lcUser,$lcObj,$lcTemplate);
+	} else {
+		if (method_exists($service,$event."Run")) {
+			
+			$event_full = $event. 'Run';
+			$_db = DB::getHandle();
+			$service->$event_full($_db,&$lcUser,&$lcObj,&$lcTemplate);
+			
+		} else { 
+			$lcTemplate[message] = "The function <i>$event</i> does not exist.";
+			lcSystem::systemPreTemplate($lcObj,$lcTemplate);
+			$service->errorMessage($lcObj, $lcTemplate);
+			closepage();
+		}
+	}
+	
+
+		/*
+		 * Call the template function of the main
+		 * service class.  Can be overridden for 
+		 * custom templates per service, or replaced 
+		 * in 'service.php' with a different template 
+		 * system.
+		 *
+		 * Need to create the TEMPLATE_URL constant here
+		 *
+		 */
+
+$handler = $service->presentor;
+// go inside the class if the keyword is 'native'
+if ($handler == 'native') { $handler = 'presentation'; }
+	// debugging
+if  ( ($handler == '' ) || ($handler == 'debug') ) {
+	$lcObj->templateStyle = 'debug';
+	lcSystem::systemPreTemplate($lcObj,$lcTemplate);
+	debugPresentation($lcObj,$lcTemplate);
+} else {
+	// templatize
+                //call the system pretemplate function
+                lcSystem::systemPreTemplate($lcObj,$lcTemplate);
+
+                //call the module pretemplate function
+                $service->preTemplate($lcObj,$lcTemplate);
+
+                // templatize
+                if ( method_exists($service,$handler) ) {
+                        $service->$handler($lcObj,$lcTemplate);
+                } else {
+                        $handler($lcObj,$lcTemplate);
+                }
+}
+
+
+//xdebug_dump_function_profile(5);
+//xdebug_stop_profiling();
+closepage();
+
+?>
