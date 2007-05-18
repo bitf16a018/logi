3c4c0d061c75bdc25398fb927e7d6b3baa8a30d0
Arslan Ahmad <47880584+bitf16a018@users.noreply.github.com>
2007-05-18 02:25:25 +0000

    Adding ability to view image content types in the lesson viewer.

--- logicampus/trunk/src/logicreate/lib/lc_lob.php
+++ logicampus/trunk/src/logicreate/lib/lc_lob.php
@@ -2,6 +2,39 @@
 
 class LC_Lob {
 
+
+	/**
+	 * @static
+	 */
+	function getMimeIcon($mime) {
+		switch ($mime) {
+			case 'text/html':
+				return 'html.png';
+				break;
+			default:
+				return 'document.png';
+				break;
+		}
+	}
+
+
+	function getMimeForSubtype($subType,$ext='') {
+		if ($ext == 'jpeg' || $ext == 'pjpeg' || $ext == 'jpg') {
+			$ext = 'jpeg';
+		}
+
+		switch($subType) {
+			case 'text':
+				return 'text/plain';
+			case 'wiki':
+				return 'text/wiki';
+			case 'html':
+				return 'text/html';
+			case 'image':
+				return 'image/'.$ext;
+		}
+
+	}
 }
 
 ?>
