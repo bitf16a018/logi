4ab7a1de41d7bfca2195b767e11e6d453095db70
hardcorelamer <hardcorelamer@6eb2142d-0950-0410-ae72-a70787517d56>
2007-09-12 03:01:00 +0000

    Adding some sample data to wipe clean.  Form and FormInfo is always needed in an install.

--- logicampus/trunk/data/wipe_clean.sh
+++ logicampus/trunk/data/wipe_clean.sh
@@ -34,12 +34,16 @@ echo 'lcPerms'
 mysql -u $1 -p$2 $db < ../data/lcPerms.sql 
 echo 'lcForms'
 mysql -u $1 -p$2 $db < ../data/lcForms_and_lcFormInfo.sql 
+mysql -u $1 -p$2 $db < ../data/lcForms.sql 
+mysql -u $1 -p$2 $db < ../data/lcFormInfo.sql 
 echo 'lcConfig'
 mysql -u $1 -p$2 $db < ../data/lcConfig.sql
 echo 'lcGroups'
 mysql -u $1 -p$2 $db < ../data/lcGroups.sql
 echo 'class_enrollment'
 mysql -u $1 -p$2 $db < ../data/class_enrollment.mysql.sql
+echo 'class_lesson_sequence'
+mysql -u $1 -p$2 $db < ../data/class_lesson_sequence.mysql.sql
 echo '***********************'
 echo 'INSTALLING base modules'
 sh ./install_metainfo.sh $1 $2 $db
