141b06424027ab1140e3ed9443e858e4e59686c9
hardcorelamer <hardcorelamer@6eb2142d-0950-0410-ae72-a70787517d56>
2007-09-12 13:05:06 +0000

    Don't allow char 0x7E in URLs, i forget what it is, but it's not allowed. Oh, I think it's forward slash (solidus).

--- logicampus/trunk/src/logicreate/lib/lc_lob.php
+++ logicampus/trunk/src/logicreate/lib/lc_lob.php
@@ -226,7 +226,7 @@ class LC_Lob {
 		$ret = str_replace('&', ' and ', $name);
 		$ret = str_replace(' ', '_', $ret);
 
-		$pattern = '/[\x{21}-\x{2C}]|[\x{2F}]|[\x{5B}-\x{5E}]/';
+		$pattern = '/[\x{21}-\x{2C}]|[\x{2F}]|[\x{5B}-\x{5E}]|[\x{7E}]/';
 		$ret = preg_replace($pattern, '_', $ret);
 		$ret = str_replace('___', '_', $ret);
 		$ret = str_replace('__', '_', $ret);
