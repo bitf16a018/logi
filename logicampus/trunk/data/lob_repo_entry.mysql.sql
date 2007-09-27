557fcaacc9c0527ae0326ec0b6aba6e05bece82f
hardcorelamer <hardcorelamer@6eb2142d-0950-0410-ae72-a70787517d56>
2007-09-27 16:12:47 +0000

    Fixing some field lengths and indexes for lob repo.

--- logicampus/trunk/data/lob_repo_entry.mysql.sql
+++ logicampus/trunk/data/lob_repo_entry.mysql.sql
@@ -1,7 +1,7 @@
 -- Dumping SQL for project logicampus
 -- entity version: 0.0
 -- DB type: mysql
--- generated on: 09.26.2007
+-- generated on: 09.27.2007
 
 
 DROP TABLE IF EXISTS `lob_repo_entry`;
@@ -11,11 +11,14 @@ CREATE TABLE `lob_repo_entry` (
 	`lob_guid` varchar (255) NOT NULL, 
 	`lob_title` varchar (255) NOT NULL, 
 	`lob_type` varchar (100) NOT NULL, 
-	`lob_sub_type` varchar (255) NOT NULL, 
+	`lob_sub_type` varchar (100) NOT NULL, 
 	`lob_mime` varchar (100) NOT NULL, 
-	`lob_description` text NOT NULL, 
-	`lob_notes` text NOT NULL, 
+	`lob_description` text, 
+	`lob_notes` text, 
 	`lob_urltitle` varchar (255) NOT NULL, 
 	`lob_version` integer (11) NOT NULL,
 	PRIMARY KEY (lob_repo_entry_id) 
 );
+
+CREATE INDEX lob_guid_idx ON lob_repo_entry (lob_guid);
+
