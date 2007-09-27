98f41cd4633bc89052d9131c9301d7e49921fd5e
hardcorelamer <hardcorelamer@6eb2142d-0950-0410-ae72-a70787517d56>
2007-09-27 15:08:22 +0000

    Adding simpletest based unit test service to default install. Prohibiting unitTest from being packaged.

---
+++ logicampus/trunk/src/logicreate/services/unitTest/unitTest.php
@@ -0,0 +1,52 @@
+<?
+
+class unitTest extends NoAuth {
+
+	var $test = array();
+	var $results = array();
+	var $presentor = 'EmptyPresentation';
+	var $authorizer = 'noAuth';
+
+	function run(&$db, &$u, &$lc, &$t) {
+
+		$lc->templateName = 'unitTest';
+		while ( list($k,$v ) = @each($this->tests) ) {
+			$test = '$this->results[\''.$k.'\'] = $this->'.$v['name'].'Test();';
+			eval($test);
+			//print "testing $k $v <br>";
+		}
+
+
+		while ( list($k,$v ) = @each($this->results) ) {
+			$t['results'][$k] = $v == $this->tests[$k]['expected']? 'PASS':'FAIL';
+		}
+
+		$t['s'] = $lc->getvars[0];
+		$t['m'] = 'unitTest';
+	}
+
+	/**
+	 * show details about one particular test case
+	 */
+	/*
+	function inspectRun(&$db, &$u, &$lc, &$t) {
+
+		$testName = $lc->getvars['t'];
+		$moduleName = $lc->getvars['m'];
+		$serviceName = $lc->getvars['s'];
+		$file = fopen(SERVICES_PATH.$moduleName.'/'.$serviceNames,'r+');
+		print "<pre>\n";
+		while (!feof($file) ) {
+			$line = fgets($file);
+			while(list($k,$v) = each($this->tests) ) {
+				if ( strpos($line,$v['name']) ) {
+					echo '<a name="'.$k.'"/>';
+				}
+			}
+			echo $line . " == ";
+		}
+		print "</pre>\n";
+	}
+//	*/
+}
+?>
